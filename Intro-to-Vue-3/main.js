fb88366ee291606c284e109a4f59b4718ffec803
adriano-sick <sick.zero.boy@gmail.com>
2021-10-05 00:53:24 -0300

    implements recommend and fixes bugs

--- Intro-to-Vue-3/main.js
+++ Intro-to-Vue-3/main.js
@@ -1,15 +1,14 @@
 const app = Vue.createApp({
     data(){
         return {
-            cart:0,
-            product: 'Dark Chocolate',
-            image: './assets/images/dark_choc.jpg',
-            inStock: true,
-            details: ['60% cocoa', '30% sugar', '10% chocolate butter'],
-            variants: [
-                { id: 2234, type:'dark', image: './assets/images/dark_choc.jpg' },
-                { id: 2235, type:'white', image: './assets/images/white_choc.jpg' }
-            ]
+            cart:[],
+            premium: false
+        }
+    },
+    methods:{
+
+        updateCart(id){
+            this.cart.push(id)
         }
     }
 })
